fd37b1df3ac446059d042d97444304754346ba53
ECHO-MSI\Erwan <erwan.baccon@gmail.com>
2018-02-25 11:46:04 +0100

    [EBA] - Create the script for one fight only (for the moment not in the loop for launch the fight automaticaly)

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define _WIN32_WINNT 0x0500
 #include <windows.h>
 
-void clic_ici(int x, int y)
+void click_here(int x, int y)
 {
     INPUT i[2];
     memset(i, 0, sizeof(i));
@@ -16,18 +16,81 @@ void clic_ici(int x, int y)
     SendInput(2, i, sizeof(INPUT));
 }
 
-void clic_charge()
+void click_charge()
 {
-    clic_ici( 100, 550);
+    click_here( 100, 560);
+}
+
+void click_shield()
+{
+    click_here( 175, 560);
+}
+
+void click_potion()
+{
+    click_here( 250, 560);
+}
+
+void click_close_purpose_heal()
+{
+    click_here( 175, 450);
+}
+
+void click_go()
+{
+    click_here( 250, 400);
+}
+
+// Fight while loop time and delay
+void fight(int time, int delay)
+{
+    int time_to_fight = 0;
+    while(time_to_fight < time){
+        time_to_fight++;
+        click_charge();
+        click_shield();
+        click_potion();
+        Sleep(delay);
+    }
+}
+
+void lauch_fight(int loop, int delay)
+{
+    // Start of fight while 10 loop of 6 seconds for exemple
+        fight(loop, delay);
+        Sleep(delay);
+        // Close the pop-up and dialog
+        click_close_purpose_heal();
+        click_close_purpose_heal();
+        click_close_purpose_heal();
+        click_close_purpose_heal();
+        Sleep(delay);
+        // Click on GO button
+        click_go();
+        Sleep(delay);
+        // Start of fight while 10 loop of 6 seconds for exemple
+        fight(loop, delay);
+        Sleep(delay);
+        // Close win pop-up
+        click_close_purpose_heal();
+        Sleep(delay);
+        Sleep(delay);
+        Sleep(delay);
 }
 
 int main(void)
 {
-    int delai_en_secondes =  5;
-    int position_x = 300, position_y = 600;
+    int const delay_in_ms =  6 * 1000; // The delay must be in ms
+    int const loop =  10; // The number of loop
     while(1){
-        clic_charge();
-        Sleep(delai_en_secondes * 1000);
+        // TODO Switch to screen in the left
+
+        // TODO launch the selection of quest
+
+        // TODO Click on the quest you want
+
+        // Launch fight
+        lauch_fight(loop, delay_in_ms);
     }
     return 0;
 }
