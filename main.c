8faf59648d4b9e20d55df34e303a23c1a5b59781
ECHO-MSI\Erwan <erwan.baccon@gmail.com>
2018-02-25 16:19:50 +0100

    [EBA] - Loop for launch the fight automaticaly with drag in the left

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define _WIN32_WINNT 0x0500
 #include <windows.h>
 
+// Just click on screen
 void click_here(int x, int y)
 {
     INPUT i[2];
@@ -14,6 +15,41 @@ void click_here(int x, int y)
     i[0].mi.dwFlags = MOUSEEVENTF_MOVE|MOUSEEVENTF_ABSOLUTE|MOUSEEVENTF_LEFTDOWN;
     i[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
     SendInput(2, i, sizeof(INPUT));
+}
+
+// Click and drag to another screen
+void click_drag_screen(int x, int y, int xDrag, int yDrag, int delay)
+{
+    INPUT i[1];
+    INPUT w[1];
+    INPUT z[1];
+    memset(i, 0, sizeof(i));
+    memset(w, 0, sizeof(w));
+    memset(z, 0, sizeof(z));
+    i[0].type = INPUT_MOUSE;
+    i[0].mi.dx = (65535. / (double)GetSystemMetrics(SM_CXSCREEN)) * xDrag;
+    i[0].mi.dy = (65535. / (double)GetSystemMetrics(SM_CYSCREEN)) * yDrag;
+    i[0].mi.dwFlags = MOUSEEVENTF_MOVE|MOUSEEVENTF_ABSOLUTE|MOUSEEVENTF_LEFTDOWN;
+
+    w[0].type = INPUT_MOUSE;
+    w[0].mi.dx = (65535. / (double)GetSystemMetrics(SM_CXSCREEN)) * x;
+    w[0].mi.dy = (65535. / (double)GetSystemMetrics(SM_CYSCREEN)) * y;
+    w[0].mi.dwFlags = MOUSEEVENTF_MOVE|MOUSEEVENTF_ABSOLUTE;
+
+    z[0].type = INPUT_MOUSE;
+    z[0].mi.dx = (65535. / (double)GetSystemMetrics(SM_CXSCREEN)) * x;
+    z[0].mi.dy = (65535. / (double)GetSystemMetrics(SM_CYSCREEN)) * y;
+    z[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+    SendInput(1, i, sizeof(INPUT));
+    Sleep(1000);
+    SendInput(1, w, sizeof(INPUT));
+    Sleep(1000);
+    SendInput(1, i, sizeof(INPUT));
+    SendInput(1, w, sizeof(INPUT));
+    Sleep(1000);
+    SendInput(1, z, sizeof(INPUT));
+    Sleep(1000);
 }
 
 void click_charge()
@@ -41,6 +77,21 @@ void click_go()
     click_here( 250, 400);
 }
 
+void click_sign_quest()
+{
+    click_here( 175, 350);
+}
+
+void click_quest_1()
+{
+    click_here( 175, 200);
+}
+
+void click_quest_4()
+{
+    click_here( 175, 400);
+}
+
 // Fight while loop time and delay
 void fight(int time, int delay)
 {
@@ -50,7 +101,7 @@ void fight(int time, int delay)
         click_charge();
         click_shield();
         click_potion();
-        Sleep(delay);
+        Sleep(3000);
     }
 }
 
@@ -81,13 +132,28 @@ void lauch_fight(int loop, int delay)
 int main(void)
 {
     int const delay_in_ms =  6 * 1000; // The delay must be in ms
-    int const loop =  10; // The number of loop
+    int const loop =  40; // The number of loop
     while(1){
-        // TODO Switch to screen in the left
+        // Switch to screen in the left
+        Sleep(delay_in_ms);
+        click_drag_screen(20, 300, 350, 300, delay_in_ms);
+        Sleep(delay_in_ms);
+        click_drag_screen(20, 300, 350, 300, delay_in_ms);
+        Sleep(delay_in_ms);
+        click_drag_screen(20, 300, 350, 300, delay_in_ms);
+        Sleep(delay_in_ms);
+        click_drag_screen(20, 300, 350, 300, delay_in_ms);
+        Sleep(delay_in_ms);
+        click_drag_screen(20, 300, 350, 300, delay_in_ms);
 
-        // TODO launch the selection of quest
+        // Launch the selection of quest
+        Sleep(delay_in_ms);
+        click_sign_quest();
 
-        // TODO Click on the quest you want
+        // Click on the quest you want
+        Sleep(delay_in_ms);
+        click_quest_4();
+        Sleep(delay_in_ms);
 
         // Launch fight
         lauch_fight(loop, delay_in_ms);
